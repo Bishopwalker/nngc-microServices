57b18e7c5f2a33fc182e4799536b612997577f36
Bishop Walker <bishop.walker@microhealthllc.com>
2025-08-21 11:34:53 -0400

    first push

---
+++ token-service/src/main/java/org/nngc/token/Token.java
@@ -0,0 +1,46 @@
+package org.nngc.token;
+
+import com.northernneckgarbage.nngc.entity.Customer;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Entity
+public class Token {
+
+    @Id
+    @SequenceGenerator(
+            name = "token_id_seq",
+            sequenceName = "token_id_seq",
+            allocationSize = 1)
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "token_id_seq"
+    )
+    public long id;
+
+    public String token;
+
+    @Enumerated(EnumType.STRING)
+    public TokenType tokenType = TokenType.BEARER;
+
+    public boolean revoked;
+
+    public boolean expired;
+    public LocalDateTime createdAt;
+    public LocalDateTime expiresAt;
+
+    private LocalDateTime confirmedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    public Customer customer;
+}
