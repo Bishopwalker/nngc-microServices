8e3d07169b973a190012c06b0ae74ac794a04127
Bishop Walker <bishop.walker@microhealthllc.com>
2025-08-25 21:59:17 -0400

    Update CI/CD configuration and infrastructure files
    - Update GitLab CI configuration
    - Update API Gateway and service configurations
    - Update Docker Compose files for improved service orchestration
    - Update root POM and service registry configuration

--- RegistrationService/src/test/java/org/nngc/registrationservice/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package org.nngc.registrationservice;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
